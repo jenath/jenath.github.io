e15895c8a0d3b59b0829936ce1561c65cfed5e01
jenath <jenathkanagesan@gmail.com>
2017-11-15 16:06:20 +0000

    Add files via upload

---
+++ js/page.js
@@ -0,0 +1,11 @@
+  document.getElementById("GitHub").onclick = function () {
+        location.href = "https://github.com/jenath/jenath.github.io";
+    };
+
+  document.getElementById("FreeCodeCamp").onclick = function () {
+        location.href = "https://www.freecodecamp.org/jenath";
+    };
+
+  document.getElementById("CodeWars").onclick = function () {
+        location.href = "https://www.codewars.com/users/jenath";
+    };
